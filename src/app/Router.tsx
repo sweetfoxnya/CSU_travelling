da196583caa8130494ac81209271de5f126d2ad8
Amir <far20022002213@gmail.com>
2024-06-09 16:07:58 +0500

    auth feat: added Auth page in app

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,12 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from "react";
 
-import {Home} from "@pages";
+import {Auth, Home} from "@pages";
 
 export const Router = () => (
   <BrowserRouter basename="/">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
       </Routes>
   </BrowserRouter>
 );
