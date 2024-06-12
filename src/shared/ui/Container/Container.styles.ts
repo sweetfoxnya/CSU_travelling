acac7a1b9deee522697d28f0b7408d8381847a9a
Amir <far20022002213@gmail.com>
2024-06-12 18:15:08 +0500

    form feat: added centering content in Container

--- src/shared/ui/Container/Container.styles.ts
+++ src/shared/ui/Container/Container.styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    
     width: 100%;
     max-width: 1440px;    
     
