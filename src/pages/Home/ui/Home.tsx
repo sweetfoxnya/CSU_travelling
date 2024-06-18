12b31b396dfddfa60bb7fedbd888563c7defbb61
Amir <far20022002213@gmail.com>
2024-06-18 15:41:37 +0500

    chore: added Header in home page

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {observer} from "mobx-react";
 
 import {Container} from "@shared";
+import {Header} from "@widgets/Header";
 
 export const Home = observer(() => {
   return (
     <Container>
+      <div style={{background: 'black'}}>
+        <Header />
+      </div>
     </Container>
   );
 });
